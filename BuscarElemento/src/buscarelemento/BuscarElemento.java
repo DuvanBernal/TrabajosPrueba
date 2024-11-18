1f898241a25e3a5129af8d7ba37f0e995011c7c1
DuvanBernal <hbernal@jdc.edu.co>
2024-11-18 00:50:20 -0500

    Add files via upload

---
+++ BuscarElemento/src/buscarelemento/BuscarElemento.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package buscarelemento;
+
+/**
+ *
+ * @author duvan
+ */
+public class BuscarElemento {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ BuscarElemento/src/buscarelemento/Nodo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package buscarelemento;
+
+/**
+ *
+ * @author duvan
+ */
+public class Nodo {
+
+    private Nodo cabeza;
+
+    public int buscarelemento(int elemento) {
+
+        Nodo actual = cabeza;
+        while (actual != null) {
+
+        }
+        return -1;
+
+    }
+
+}
