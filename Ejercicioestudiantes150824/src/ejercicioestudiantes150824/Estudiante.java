93dc29be9a39cd199513b1019c99c4f027ad11f6
DuvanBernal <hbernal@jdc.edu.co>
2024-11-18 00:51:34 -0500

    Add files via upload

---
+++ Ejercicioestudiantes150824/src/ejercicioestudiantes150824/Estudiante.java
@@ -0,0 +1,44 @@
+package ejercicioestudiantes150824;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author duvan
+ */
+public class Estudiante {
+
+    String Nombre, Apellido, Carrera;
+    int edad;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+    public String getCarrera(){
+        return Carrera;
+    }
+    public void setCarrera(String Carrera){
+        this.Carrera=Carrera;
+    }
+    public int getedad(){
+        return edad;
+    }
+    public void setedad(int edad ){
+        this.edad=edad;
+    }
+
+}
---
+++ ejer.1/src/ejer/pkg1/NewMain.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejer.pkg1;
+
+/**
+ *
+ * @author 57313
+ */
+public class NewMain {
+
+    
+    public static void main(String[] args) {
+        System.out.println("HOLA");
+    }
+    
+}
